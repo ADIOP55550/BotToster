ba1e086550002789c5790f7099fd02f3f9b3a4aa
przemykomo <przemyslawsowa15@gmail.com>
2018-05-15 15:59:21 +0200

    s

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,10 +1,15 @@
 // Komenda pomocy
 const fs = require('fs');
+const firstline = require('firstline')
 exports.run = (client, message, args) => {
+
+    let message;
 
     fs.readdir("./", (err, files) => {
         files.forEach(file => {
-            console.log(file);
+            message += file + firstline(file);
         });
     })
+
+    message.channel.send(message);
 }
