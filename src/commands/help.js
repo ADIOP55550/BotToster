0ed9fa68a30c37abcbd916bbe7e7d11ed956b7a1
przemykomo <przemyslawsowa15@gmail.com>
2018-05-16 20:23:14 +0200

    first line

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,12 +1,11 @@
 // Komenda pomocy
 const fs = require('fs');
-const firstline = require('firstline')
 exports.run = (client, message, args) => {
 
     let files = "";
 
     fs.readdirSync("./src/commands/").forEach(file => {
-        files += file.substring(0, file.length - 3) + " " + firstline(file) + "\n"
+        files += file.substring(0, file.length - 3) + " " + fs.readFileSync(file).split("\n").shift() + "\n";
     })
 
     message.channel.send(files);
