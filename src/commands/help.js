bba29288561c8fcf3ce2104f0ae0a425454e2f8a
przemykomo <przemyslawsowa15@gmail.com>
2018-05-16 20:15:31 +0200

    d

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,7 +6,7 @@ exports.run = (client, message, args) => {
     let files;
 
     fs.readdirSync("./src/commands/").forEach(file => {
-        files += file.substring(0, file.length - 3) + "testowy napis\n"
+        files += file.substring(0, file.length - 3) + " testowy napis\n"
     })
 
     message.channel.send(files);
