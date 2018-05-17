6255b757f390ca9c1517ed72b76246b895ea0de9
przemykomo <przemyslawsowa15@gmail.com>
2018-05-17 15:53:53 +0200

    off

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const fs = require('fs');
 const botT = require('./src/bot/botT.js');
 
+while(true) {
+    
+}
+
 let BToster = new botT(JSON.parse(fs.readFileSync('config.json')));
 BToster.init();
 BToster.start();
