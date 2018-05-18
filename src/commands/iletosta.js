a5289e1e3e890fb16fa00e4fb87bb39ef819da35
przemykomo <przemyslawsowa15@gmail.com>
2018-05-18 13:51:42 +0200

    args

--- src/commands/iletosta.js
+++ src/commands/iletosta.js
@@ -1,5 +1,5 @@
 // sprawdza ile w wyrażeniu jest tosta
 const ss = require('string-similarity');;
 exports.run = (client, message, args, config) => {
-    message.channel.send(ss.compareTwoStrings(message.content, "tost"));
+    message.channel.send(ss.compareTwoStrings(args, "tost"));
 }
