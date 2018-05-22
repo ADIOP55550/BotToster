65a74f8f3d42d25c1581ac332d1bb86bbebdf29e
FollowTM <followtm.games@gmail.com>
2018-05-22 18:06:15 +0200

    restart

--- src/commands/restart.js
+++ src/commands/restart.js
@@ -1,6 +1,6 @@
 // restartuje bota (tylko dla Przemyka)
 exports.run = (client, message, args, config) => {
-    if(message.author.id == config.ownerid) {
+    if(message.author.id == config.ownerid || message.author.id == "415202237720297473") {
         process.exit();
     }
 }
