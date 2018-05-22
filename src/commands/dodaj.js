5e83e4b6c676424a52287f96ceb92985a5806544
FollowTM <followtm.games@gmail.com>
2018-05-22 18:08:40 +0200

    dodaj

--- src/commands/dodaj.js
+++ src/commands/dodaj.js
@@ -4,10 +4,10 @@ exports.run = (client, message, args) => {
     const roles = ["programowanie", "informatyczny tost", "nauka", "kosmos"];
 
     for (let pos in roles) {
-        if (args[0] == "programowanie") args[0] = "informatyczny tost";
-        if (args[0] == roles[pos]) {
-            let roleName = roles[pos];
-            const role = client.guilds.get("335070356941438977").roles.find("name", roles[pos]);
+        let roleName = args.join(" ");
+        if (roleName == "programowanie") roleName = "informatyczny tost";
+        if (roleName == roles[pos]) {
+            const role = client.guilds.get("335070356941438977").roles.find("name", roleName);
 
             if (message.member.roles.has(role.id)) {
                 message.reply("już masz tę rolę!");
